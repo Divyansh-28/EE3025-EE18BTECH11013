10f858db8926b8bf67165538a7b8aff2dea47e0c
Divyansh <divyanshmaduriya02@gmail.com>
2021-06-12 14:03:42 +0530

    updates

--- codes/EE18BTECH11013_A1.py
+++ codes/EE18BTECH11013_A1.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 def plot(L, Fn, St):
 	for i in range(len(L)):
 		s=0
+		n=np.arange(6)
+		c=complex(0,1)
 		for j in range(len(L)):
 			s=s + 	L[j]*np.exp((2*np.pi*i*j*(-c))/len(L))
 		Fn[i]=s
@@ -20,19 +22,20 @@ def plot(L, Fn, St):
 	plt.show()
 		
 #plot of X(n)
-n=np.arange(6)
-c=complex(0,1)
 X=np.array([1,2,3,4,2,1])
+print(X)
 Xn=np.zeros(len(X),dtype='complex')
+print(Xn)
 plot(X,Xn,'X(n)')
 		
 #plot of H(n)
-n=np.arange(15)
-u1=np.ones(15)
-u2=np.array([0,0,1,1,1,1,1,1,1,1,1,1,1,1,1])
-H=np.zeros(15)
+u1=np.ones(6)
+print(u1)
+u2=np.array([0,0,1,1,1,1])
+H=np.zeros(6)
+print(H)
 s=0
-for i in range(15):
+for i in range(6):
     H[i]=((-0.5)**i)*u1[i] + ((-0.5)**(i-2))*u2[i]
 Hn=np.zeros(len(H),dtype='complex')
 
